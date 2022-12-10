95435efab23c44828bf632e27c487a2dbe4572e3
selviaindriani <iselvia27@gmail.com>
2022-12-10 09:18:25 +0700

    new update

--- app/Http/Controllers/KlaimController.php
+++ app/Http/Controllers/KlaimController.php
@@ -95,7 +95,7 @@ class KlaimController extends Controller
             'mm_akhir' => 'required|lte:mm_awal',
             'no_seri' => 'required',
             'tahun_produksi' => 'required',
-            'images' => 'required|array|between:3,6'
+            'images' => 'required|array|between:3,6||mimes:jpg,jpeg,png'
         ]);
 
         $cust = Customer::find($request->customer_id);
